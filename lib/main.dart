6a1e247f7c62bd8fe2fb3c5ce7c40ae37ffefefd
ThucNV <nvthuc@outlook.com>
2019-09-16 17:07:18 +0700

    Step 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,17 +19,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
+    return Center(
+      child: Row(children: <Widget>[
         Expanded(
-            child: Image(
-          image: AssetImage("images/dice1.png"),
-        )),
+          child: FlatButton(
+              child: Image.asset("images/dice1.png"),
+            onPressed: () {
+              print("Pressed Right button");
+            },
+          ),
+        ),
         Expanded(
-            child: Image(
-          image: AssetImage("images/dice1.png"),
-        ))
-      ],
+          child: FlatButton(
+              child: Image.asset("images/dice1.png")
+            , onPressed: () {
+                print("Pressed Right button");
+          },
+          ),
+        ),
+      ]),
     );
   }
 }
