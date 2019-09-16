7a5db9ba7c19e2b3d48ce87b35124511a5c41433
thucnv <nvthuc@outlook.com>
2019-09-16 22:52:01 +0700

    random image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,25 +18,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(children: <Widget>[
         Expanded(
           child: FlatButton(
-              child: Image.asset("images/dice1.png"),
+            child: Image.asset("images/dice$leftDiceNumber.png"),
             onPressed: () {
-              print("Pressed Right button");
+              setState(() {
+                leftDiceNumber = Random().nextInt(5) + 1;
+                print("Pressed left button [$leftDiceNumber]");
+              });
             },
+//            onPressed: onLeftTap,
           ),
         ),
         Expanded(
           child: FlatButton(
-              child: Image.asset("images/dice1.png")
-            , onPressed: () {
-                print("Pressed Right button");
-          },
+            onPressed: () {
+              setState(() {
+                rightDiceNumber = Random().nextInt(5) + 1;
+                print("Pressed right button [$rightDiceNumber]");
+              });
+            },
+            child: Image.asset("images/dice$rightDiceNumber.png"),
           ),
         ),
       ]),
